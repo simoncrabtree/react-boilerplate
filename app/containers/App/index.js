c00b34e36cce4a62c4ca98cc416bcb3a42231ead
simoncrabtree <simon@crabsticks.net>
2017-02-11 14:29:43 +0000

    Extracted ShoppingList page into separate container

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link, RelativeFragment as Fragment } from 'redux-little-router'
+import ShoppingList from '../ShoppingList'
 
-const mapState = (state) => {
-  console.log(state)
-  return {
-    pageTitle: state.title,
-    shoppingListInputValue: state.shoppingList.inputValue,
-    items: state.shoppingList.items
-  }
-}
+const mapState = (state) => state
 
-const mapActions = (dispatch) => {
-  return {
-    onChange: e => dispatch({type: 'SHOPPINGLIST_INPUT_CHANGE', value: e.target.value }),
-    onSubmit: e => {
-      e.preventDefault()
-      dispatch({type: 'SHOPPINGLIST_ITEM_ADD'})
-    }
-  }
-}
-
-const AppPage = ({pageTitle, shoppingListInputValue, items, onChange, onSubmit}) =>
+const AppPage = ({ pageTitle }) =>
   <div>
     <Fragment key='/' forRoute='/'>
       <Link href='/home'>Home</Link>
@@ -32,22 +16,11 @@ const AppPage = ({pageTitle, shoppingListInputValue, items, onChange, onSubmit})
       <p>Home</p>
     </Fragment>
     <Fragment key='/shopping' forRoute='/shopping'>
-      <h1>Shopping List</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          type='text'
-          placeholder='Add item to list'
-          value={shoppingListInputValue}
-          onChange={onChange}
-        />
-      </form>
-    <div>
-      {items.map((item) => <div key={item.name}>{item.name}</div>)}
-    </div>
+      <ShoppingList />
     </Fragment>
     <Fragment key='/cupboard' forRoute='/cupboard'>
       <p>Cupboard</p>
     </Fragment>
   </div>
 
-export default connect(mapState, mapActions)(AppPage)
+export default connect(mapState)(AppPage)
---
+++ app/containers/ShoppingList/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const mapState = (state) => {
+  return {
+    items: state.shoppingList.items,
+    shoppingListInputValue: state.shoppingList.inputValue
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    onChange: e => dispatch({type: 'SHOPPINGLIST_INPUT_CHANGE', value: e.target.value}),
+    onSubmit: e => {
+      e.preventDefault()
+      dispatch({type: 'SHOPPINGLIST_ITEM_ADD'})
+    }
+  }
+}
+
+const ShoppingListPage = ({ shoppingListInputValue, items, onChange, onSubmit }) =>
+<div>
+  <h1>Shopping List</h1>
+  <form onSubmit={onSubmit}>
+    <input 
+      type='text'
+      placeholder='Add item to list'
+      value={shoppingListInputValue}
+      onChange={onChange}
+    />
+  </form>
+  <div>
+    {items.map((item) => <div key={item.name}>{item.name}</div>)}
+  </div>
+</div>
+
+export default connect(mapState, mapDispatch)(ShoppingListPage)
