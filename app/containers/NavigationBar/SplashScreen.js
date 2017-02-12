89a45a6e33667224de95d557d42939d4ae996332
simoncrabtree <simon@crabsticks.net>
2017-02-12 12:58:34 +0000

    Introduced styled-components

---
+++ app/containers/NavigationBar/SplashScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SplashScreen = styled.div`
+  text-align: center
+`
+
+export default () =>
+  <SplashScreen>
+    <h1>
+      Welcome!
+    </h1>
+    <p>
+      Sign In or Sign Up
+    </p>
+  </SplashScreen>
--- app/containers/NavigationBar/index.js
+++ app/containers/NavigationBar/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Navbar, Nav, NavItem} from 'react-bootstrap'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import SplashScreen from './SplashScreen'
 
-const mapState = state => state
+const mapState = state => {
+  console.log(state)
+  return {
+    router: state.router
+  }
+}
 
 const mapDispatch = dispatch => {
   return {
@@ -10,20 +16,23 @@ const mapDispatch = dispatch => {
   }
 }
 
-const NavigationBar = ({ navigateTo }) => 
-  <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href='#' onClick={navigateTo('/home')}>cubdi</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem onClick={navigateTo('/shopping')}>Shopping</NavItem>
-        <NavItem onClick={navigateTo('/cupboard')}>Cupboard</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+const NavigationBar = ({ router, navigateTo }) =>
+  <div>
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href='#' onClick={navigateTo('/home')}>cubdi</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem onClick={navigateTo('/shopping')}>Shopping</NavItem>
+          <NavItem onClick={navigateTo('/cupboard')}>Cupboard</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    {router.route === '/' ? <SplashScreen /> : null}
+  </div>
 
 export default connect(mapState, mapDispatch)(NavigationBar)
